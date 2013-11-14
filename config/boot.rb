ac18080160f246d17e8e9c88e97f65e19c42fb82
Willy Mene <wmene@stanford.edu>
2013-11-13 16:17:02 -0800

    More prod prep
    - use latest dor-services gem
    - create USAGE string for each script

--- config/boot.rb
+++ config/boot.rb
@@ -5,7 +5,7 @@ require 'optparse'
 # Load the environment file based on Environment.  Default to development
 options = {:env => 'development'}
 optsparse = OptionParser.new do |opts|
-  opts.banner = "Usage: object-merge.rb [options] ids-to-merge.csv"
+  opts.banner = USAGE
 
   opts.on("-e", "--environment ENV", "Environment to run in (development, test, production). Defaults to development") do |env|
     options[:env] = env
